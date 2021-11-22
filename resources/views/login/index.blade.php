ee2e4671dfc482da303b1c78048f23ea89f59d90
muhammadluttfy <muhammadlutfi.storage@gmail.com>
2021-11-22 22:13:15 +0700

    16, User Login & Middleware

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -13,21 +13,36 @@
                     </div>
                 @endif
 
+                @if (session()->has('loginError'))
+                    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                        <strong>Login error!</strong> {{ session('loginError') }}
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    </div>
+                @endif
 
-                <script>
-                    $(".alert").alert();
-                </script>
+
+
                 <h1 class="h3 mb-3 fw-normal text-center fw-bold">Please Log in</h1>
                 <form action="/login" method="post">
 
+                    @csrf
+
                     <div class="form-floating">
-                        <input type="email" name="email" class="form-control rounded-0 rounded-top" id="email"
-                            placeholder="name@example.com">
+                        <input type="email" name="email"
+                            class="form-control rounded-0 rounded-top @error('email')
+                            is-invalid
+                        @enderror"
+                            id="email" placeholder="name@example.com" value="{{ old('email') }}" autofocus required>
                         <label for="email">Email address</label>
+                        @error('email')
+                            <div class="invalid-feedback mb-2">
+                                {{ $message }}
+                            </div>
+                        @enderror
                     </div>
                     <div class="form-floating">
                         <input type="password" name="password" class="form-control rounded-0 rounded-bottom" id="password"
-                            placeholder="Password">
+                            placeholder="Password" required>
                         <label for="password">Password</label>
                     </div>
 
